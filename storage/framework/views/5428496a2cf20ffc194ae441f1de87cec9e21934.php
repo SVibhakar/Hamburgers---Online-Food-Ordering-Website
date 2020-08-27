abe977be77dcb8d564999b8c1946a4edd2342006
Sejal Vibhakar <33162003+SVibhakar@users.noreply.github.com>
2020-08-26 22:12:44 -0700

    Add files via upload

---
+++ storage/framework/views/5428496a2cf20ffc194ae441f1de87cec9e21934.php
@@ -0,0 +1,6 @@
+<h1>Profile</h1>
+
+<h3><?php echo e(session('data')); ?></h3>
+
+
+<?php /**PATH C:\Users\Prayag\Desktop\project5\resources\views/profile.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/64fc341a0f34d5f5ce560c21b44de2b0c4b7c337.php
@@ -0,0 +1,6 @@
+<form action="login" method="post">
+<input type="text" name="user">
+<input type="password" name="password">
+<?php echo csrf_field(); ?>
+<input type="submit" name="login" value="login">
+</form><?php /**PATH C:\Users\Prayag\Desktop\project5\resources\views/login.blade.php ENDPATH**/ ?>
